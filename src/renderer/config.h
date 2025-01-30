1c0350468b1a8a784d71397ee499c1822f7274e1
rejdeboer <rick.deboer@live.nl>
2025-01-30 13:10:00 +0100

    chore: make sure config.h is compiled once

--- src/renderer/config.h
+++ src/renderer/config.h
@@ -1,3 +1,5 @@
+#ifndef RENDERER_CONFIG_H
+
 #include "context.hpp"
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_vulkan.h>
@@ -6,3 +8,6 @@
 #include <vulkan/vulkan_core.h>
 
 VulkanContext vulkan_initialize(SDL_Window *window);
+
+#define RENDERER_CONFIG_H
+#endif
