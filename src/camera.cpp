8822575567991a73af0476c08dc6355ebe78e282
Rick de Boer <rick.deboer@live.nl>
2025-02-25 22:18:58 +0100

    feat: camera rotation functions

--- src/camera.cpp
+++ src/camera.cpp
@@ -45,3 +45,22 @@ void Camera::processSDLEvent(SDL_Event &e) {
         pitch -= (float)e.motion.yrel / 200.f;
     }
 }
+
+glm::mat4 Camera::get_view_matrix() {
+    // to create a correct model view, we need to move the world in opposite
+    // direction to the camera
+    // so we will create the camera model matrix and invert
+    glm::mat4 cameraTranslation = glm::translate(glm::mat4(1.f), position);
+    glm::mat4 cameraRotation = get_rotation_matrix();
+    return glm::inverse(cameraTranslation * cameraRotation);
+}
+
+glm::mat4 Camera::get_rotation_matrix() {
+    // fairly typical FPS style camera. we join the pitch and yaw rotations into
+    // the final rotation matrix
+
+    glm::quat pitchRotation = glm::angleAxis(pitch, glm::vec3{1.f, 0.f, 0.f});
+    glm::quat yawRotation = glm::angleAxis(yaw, glm::vec3{0.f, -1.f, 0.f});
+
+    return glm::toMat4(yawRotation) * glm::toMat4(pitchRotation);
+}
