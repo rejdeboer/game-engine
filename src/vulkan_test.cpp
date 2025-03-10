e1000ed644558b45095026c7927230c15b64b61f
rejdeboer <rick.deboer@live.nl>
2025-03-10 10:53:22 +0100

    feat: game class

--- src/vulkan_test.cpp
+++ src/vulkan_test.cpp
@@ -1,69 +1,8 @@
-#include "imgui_impl_sdl3.h"
-#include "main.h"
-#include "renderer/renderer.hpp"
-#include <SDL3/SDL_render.h>
-#include <cassert>
-#include <cstdio>
-#include <cstdlib>
-#include <vulkan/vulkan.h>
-
-Uint64 TIMESTEP_MS = 1000 / 60;
-float TIMESTEP_S = (float)TIMESTEP_MS / 1000;
-
-SDL_Window *gWindow = NULL;
+#include "game.h"
 
 int main(int argc, char *args[]) {
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
-        fprintf(stderr, "could not initialize sdl3: %s\n", SDL_GetError());
-        return 1;
-    }
-    gWindow = SDL_CreateWindow("hello_sdl3", SCREEN_WIDTH, SCREEN_HEIGHT,
-                               SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE);
-    if (gWindow == NULL) {
-        fprintf(stderr, "could not create window: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    GameState state;
-    Renderer vk_renderer = Renderer(gWindow);
-    Camera camera;
-    camera.position = glm::vec3(30.f, -00.f, -085.f);
-
-    Uint64 next_game_step = SDL_GetTicks();
-    Uint64 last = next_game_step;
-    Uint64 now = last;
-    SDL_Event e;
-    bool quit = false;
-    while (quit == false) {
-        last = now;
-        now = SDL_GetTicks();
-        if (next_game_step >= now) {
-            SDL_Delay(next_game_step - now);
-            continue;
-        }
-
-        while (next_game_step <= now) {
-            while (SDL_PollEvent(&e)) {
-                if (e.type == SDL_EVENT_QUIT) {
-                    quit = true;
-                }
-                ImGui_ImplSDL3_ProcessEvent(&e);
-                // TODO: Clean this up
-                // camera.processSDLEvent(e);
-                // camera.update();
-                vk_renderer.set_camera_view(camera.get_view_matrix());
-            }
-
-            next_game_step += TIMESTEP_MS;
-        }
-
-        auto cmd = vk_renderer.begin_frame();
-        vk_renderer.draw_game(cmd);
-        vk_renderer.end_frame(cmd, now - last);
-    }
-    vk_renderer.deinit();
-    SDL_DestroyWindow(gWindow);
-
-    SDL_Quit();
-    return 0;
+    Game game;
+    game.init();
+    game.run();
+    game.deinit();
 }
