02a6b41defcca04b147ae91bfc67d160edb36eaf
rejdeboer <rick.deboer@live.nl>
2025-03-13 16:44:05 +0100

    chore: remove unused vertex code

--- src/renderer/vertex.h
+++ src/renderer/vertex.h
@@ -1,6 +1,5 @@
 #ifndef RENDERER_VERTEX_H
 
-#include <array>
 #include <glm/glm.hpp>
 #include <vulkan/vulkan_core.h>
 
@@ -10,29 +9,6 @@ struct Vertex {
     glm::vec3 normal;
     float uv_y;
     glm::vec4 color;
-
-    static VkVertexInputBindingDescription get_binding_description() {
-        VkVertexInputBindingDescription desc = {};
-        desc.binding = 0;
-        desc.stride = sizeof(Vertex);
-        desc.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-        return desc;
-    }
-
-    static std::array<VkVertexInputAttributeDescription, 2>
-    get_attribute_descriptions() {
-        std::array<VkVertexInputAttributeDescription, 2>
-            attribute_descriptions{};
-        attribute_descriptions[0].binding = 0;
-        attribute_descriptions[0].location = 0;
-        attribute_descriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
-        attribute_descriptions[0].offset = offsetof(Vertex, pos);
-        attribute_descriptions[1].binding = 0;
-        attribute_descriptions[1].location = 1;
-        attribute_descriptions[1].format = VK_FORMAT_R32G32B32A32_SFLOAT;
-        attribute_descriptions[1].offset = offsetof(Vertex, color);
-        return attribute_descriptions;
-    }
 };
 
 #define RENDERER_VERTEX_H
