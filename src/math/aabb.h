f5a78f7aa87fa0d49049b1c85e7dbef9405428e4
rejdeboer <rick.deboer@live.nl>
2025-05-05 19:35:41 +0200

    feat: aabb merge utility

--- src/math/aabb.h
+++ src/math/aabb.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <glm/common.hpp>
 #include <glm/vec3.hpp>
 
@@ -8,6 +7,11 @@ struct AABB {
     glm::vec3 min;
     glm::vec3 max;
 
-    glm::vec3 calculateSize() const { return glm::abs(max - min); }
+    glm::vec3 get_size() const { return glm::abs(max - min); }
+
+    void merge(const AABB &other) {
+        min = glm::min(min, other.min);
+        max = glm::max(max, other.max);
+    }
 };
 } // namespace math
